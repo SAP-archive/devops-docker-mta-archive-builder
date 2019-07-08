a051bfb4b85c5db928ca382e7b2a45f6d2c2983a
Florian Wilhelm <florian.wilhelm02@sap.com>
2019-07-08 11:48:02 +0200

    update

--- runTests.sh
+++ runTests.sh
@@ -16,8 +16,6 @@ docker run \
   -v //var/run/docker.sock:/var/run/docker.sock \
   -v $(pwd):/workspace \
   -v /tmp \
-  -e CX_INFRA_IT_CF_PASSWORD \
-  -e CX_INFRA_IT_CF_USERNAME \
   -e BRANCH_NAME=master \
   -e CASC_JENKINS_CONFIG=/workspace/jenkins.yml \
   -e HOST=$(hostname) \
